d8c138c7075d1a65b8d36e338140191dc170af20
zizoujab <zied.jabalah@gmail.com>
2014-04-06 14:15:04 +0200

    step-2

--- http.php
+++
@@ -1,8 +0,0 @@
-<?php
-$uri = $_SERVER['REQUEST_URI'];
-$foo = $_GET['foo'];
-
-header('Content-type: text/html');
-echo '<h1>The URI requested is: '.$uri.'</h1>';
-echo '<h1> The value of the "foo" parameter is: '.$foo.'</h1>';
-?>
